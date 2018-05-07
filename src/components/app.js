ab2152f0bb21e984229757aeeb309ee74794b466
saeromcho <1perunregret@gmail.com>
2018-05-07 22:38:23 +0900

    일단 예제를 따라해보고 있다.

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,7 @@
 //미리 구현한 라우팅 모듈을 가져와서 사용하는 예제
 
-var routes = require('./routes/index');
+var routes = require('../routes/index.js');
+var users = require('../routes/users.js');
+
+app.use('/', routes);
+app.use('/users', users);
