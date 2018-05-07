ebece7c3761138befac46a2c6a588d0e55121d7c
saeromcho <1perunregret@gmail.com>
2018-05-07 22:02:28 +0900

    디렉토리 정리.

---
+++ src/components/app.js
@@ -0,0 +1,3 @@
+//미리 구현한 라우팅 모듈을 가져와서 사용하는 예제
+
+var routes = require('./routes/index');
---
+++ src/components/expressServer/expressServer.js
@@ -0,0 +1,15 @@
+
+// const express = require('express');
+// const app = express();
+// const port = 3030;
+
+// app.get('/', function(request, response) {
+//   response.send('welcome saeromCho express server!!! with node!!');
+// });
+
+// app.listen(port, function(err) {
+//   console.log('Connected port' + port);
+//   if (err) {
+//     return console.log('Found error', err);
+//   }
+// })
