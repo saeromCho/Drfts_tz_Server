1e0eea873f903bc7e801db6a6ae8ee057226dec9
saeromcho <1perunregret@gmail.com>
2018-05-26 05:14:40 +0900

    사용되는 파일들 주석제거 및 코드정리 & 나만 보는 용도로 파일 copy

--- src/expressServer_forRom/DrftsTeaserServer.1.js
+++ src/expressServer_forRom/DrftsTeaserServer.1.js
@@ -16,7 +16,25 @@ var dbConnection = mysql.createConnection({
   password: 'drfts1234',
   database: 'drfts_tz'
 });
-
+// app.use('/index', function(request, response, next) {
+//   fs.readFile(__dirname + '/index.html');
+//   var sql = "SELECT * FROM DrftsTeaser";
+//   dbConnection.query(sql, function (err, result) {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log("index페이지에서 comment 테이블 select 완료");
+//     // JSON.stringify(result);
+//     for (var i in result) {
+//       //console.log(result[i]);
+//       // parse[i] = JSON.stringify(result[i]);
+//       console.log(JSON.stringify(result[i]));
+//     }
+//     response.send(result);
+//   });
+ 
+// })
 app.get('/index/comment', function(request, response) {
   var sql = "SELECT * FROM DrftsTeaser";
   dbConnection.query(sql, function (err, result) {
@@ -25,8 +43,10 @@ app.get('/index/comment', function(request, response) {
       return;
     }
     console.log("/index/comment 페이지에서 comment 테이블 select 완료");
+    // JSON.stringify(result);
     for (var i in result) {
       console.log("하....제발..");
+      // parse[i] = JSON.stringify(result[i]);
       console.log(JSON.stringify(result[i]));
     }
     response.send(result);
@@ -34,8 +54,53 @@ app.get('/index/comment', function(request, response) {
 });
 
 app.get('/index', function (request, response) {
-  response.sendFile(__dirname + '/index.html');
-});
+  // var sql = "SELECT * FROM DrftsTeaser";
+  // dbConnection.query(sql, function (err, result) {
+  //   if (err) {
+  //     console.log(err);
+  //     return;
+  //   }
+  //   console.log("index페이지에서 comment 테이블 select 완료");
+  //   // JSON.stringify(result);
+  //   for (var i in result) {
+  //     //console.log(result[i]);
+  //     // parse[i] = JSON.stringify(result[i]);
+  //     console.log(JSON.stringify(result[i]));
+  //   }
+    // response.send(result);
+
+    // response.send({
+
+    //   message: '데이터를 추가했습니다.',
+
+    //   data: result
+
+    // });
+    //response.send({result});
+    //fs.readFile(__dirname + '/index.html', function (data) {
+      //console.log("왜 안읽히는거야..");
+      //response.send(result);
+      // response.sendFile(__dirname + '/index.html', function (data) {
+      //   data = result;
+      //   console.log("왜 언디파인드?");
+      //   console.log(JSON.stringify(data));
+      //   response.end(JSON.stringify(data));
+      //   //response.send(data);
+      //   //return JSON.stringify(data);
+      // });
+      response.sendFile(__dirname + '/index.html');
+    //});
+    // response.sendFile(__dirname + '/index.html', function(data) {
+    //   data = result;
+    //   console.log("왜 언디파인드?");
+    //   console.log(JSON.stringify(data));
+    //   return JSON.stringify(data);
+    //   // ret = JSON.stringify(rows);
+    });
+ // });
+
+
+//});
 
 app.get('/backIndex', function (request, response) {
   response.send(data);
